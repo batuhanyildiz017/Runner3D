b5727baeeea463161a9e3cd3b54e9b7472ed3a58
Kalhklk <yildiz.batuhan017@gmail.com>
2023-09-12 22:59:48 +0300

    added obstacle animations

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameManager gameManager;
+    public PlayerController controller;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Obstacle"))
         {
             Debug.Log("Engele temas etti.");
+            gameManager.LoadScene();
+        } else if (other.CompareTag("End"))
+        {
+            Debug.Log("Sona geldik.");
+            controller.runningSpeed = 0;
+            
         }
     }
 }
